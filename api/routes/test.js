9c97124b6ad40a9d81a6765981f32d0afc37c441
VictorMNSalas <victorsalasnz@gmail.com>
2024-02-29 23:41:00 -0600

    first img phase

--- api/routes/test.js
+++ api/routes/test.js
@@ -18,20 +18,20 @@ const sharp = require('sharp')
 const helperImg = (filePath, fileName, size = 300) => {
     return sharp(filePath)
         .resize(size)
-        .toFile(`../../assets/optimize/${fileName}`)
+        .toFile(`./assets/optimize/${fileName}`)
 }
 
 //diskStorage es para definir donde se guardaran las imagenes
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '../../assets/upload') //almacen de imagen cruda
+        cb(null, './assets/upload') //almacen de imagen cruda
     },
     filename: (req, file, cb) => {
         const ext = file.originalname.split('.').pop() //obtener el nombre desde el . (imagen.png => png)
 
         cb(null, `${Date.now()}.${ext}`) //cb obtiene el data y genera el doc nombrandolo 
     }
-})
+}) 
 
 const upload = multer({ storage }) 
 
