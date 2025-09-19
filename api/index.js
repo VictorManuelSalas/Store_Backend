c5b1e5e4cb4d6791558be24f88dd32c6b6c8c8ab
Víctor Manuel Nuñez Salas <98570683+VictorManuelSalas@users.noreply.github.com>
2025-09-19 09:01:36 -0600

    ips allowed

--- api/index.js
+++ api/index.js
@@ -5,15 +5,29 @@ const routerApi = require("./routes");
 const config = require("../config/config");
 const app = express();
 
+// Lista de IPs permitidas
+const allowedIps = ["123.45.67.89", "98.76.54.32", "127.0.0.1"];
+
+// Middleware para validar IP
+app.use((req, res, next) => {
+  const clientIp =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log("IP Request => " ,clientIp);
+  if (allowedIps.includes(clientIp)) {
+    next();
+  } else {
+    res.status(403).json({ message: "Access denied: IP not allowed" });
+  }
+});
+
 app.use(express.json());
-app.use(cors("*"));
+app.use(cors());
 
 const connect = require("../database");
 connect(config.dataBase.mongoURL);
 
 routerApi(app);
 
-
 app.listen(config.port, () => {
-  console.log(`Server running in the port ${config.port}`);
+  console.log(`Server running on port ${config.port}`);
 });
